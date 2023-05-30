0e8ee42cd69ceea67503aba3d07ca1d6715cca79
Kenza Bouachik <kenzabouachik@gmail.com>
2023-05-30 14:16:46 +0200

    added image_url to tables and seed

--- db/migrate/20230529144509_create_flats.rb
+++ db/migrate/20230529144509_create_flats.rb
@@ -2,6 +2,7 @@ class CreateFlats < ActiveRecord::Migration[7.0]
   def change
     create_table :flats do |t|
       t.string :name
+      t.string :image_url
       t.string :address
       t.text :description
       t.float :price
