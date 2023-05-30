ff5549d56e2449051225d9599efc497e8494cc2d
ricardo martins <ricardomfneves@gmail.com>
2023-05-30 12:26:10 +0200

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/destroy'
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/destroy'
   devise_for :users
   root to: "pages#home"
+  # get "dashboard", to: "pages#dashboard"
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
