5e0999634ec828b7779006baf7dd8df489b8dbb3
ricardo martins <ricardomfneves@gmail.com>
2023-06-01 14:26:25 +0200

    new search bar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,18 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.all
+    # @flats = Flat.all
+    if params[:query].present?
+      sql_query = <<~SQL
+        flats.name ILIKE :query
+        OR flats.description ILIKE :query
+        OR flats.address ILIKE :query
+
+
+      SQL
+      @flats = Flat.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @flats = Flat.all
+    end
   end
 
   def show
