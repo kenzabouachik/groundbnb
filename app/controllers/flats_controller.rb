7ababf7d4588b48999f1f2f41639cb6add668ce6
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-06-01 10:30:52 +0200

    Add dotenv-rails

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,9 +9,17 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to flats_path
+    else
+      redirect_to new_flat_path, status: :unprocessable_entity, alert: 'An error occured'
+    end
   end
 
   def edit
@@ -21,5 +29,11 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price, :max_people)
   end
 end
