b624ff799b802c1c74b0f5d4eb2dd42b38d91123
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-06-01 13:49:54 +0200

    last modif

--- app/javascript/ghost.js
+++ app/javascript/ghost.js
@@ -1,21 +1,27 @@
-window.addEventListener('load', function() {
+window.addEventListener('scroll', function() {
   var image = document.getElementById('centered-image');
-  var sound = document.getElementById('sound');
 
   setTimeout(function() {
     image.classList.add('enlarged'); // Ajoute la classe pour agrandir l'image
     image.style.opacity = 1; // Rend l'image visible
-    sound.play(); // Déclenche la lecture du son
-  }, 1000); // Délai avant d'agrandir l'image et jouer le son (1 seconde dans cet exemple)
+  }, 1000); // Délai avant d'agrandir l'image (1 seconde dans cet exemple)
 
   setTimeout(function() {
     image.classList.add('hidden'); // Ajoute la classe pour cacher l'image
-    image.classList.add('fade-out'); // Ajoute une classe pour démarrer la transition de l'opacité
-    sound.pause(); // Arrête la lecture du son
-  }, 4000); // Délai avant de cacher l'image et arrêter la lecture du son (4 secondes dans cet exemple)
+
+    // Ajoute une classe pour démarrer la transition de l'opacité
+    image.classList.add('fade-out');
+  }, 4000); // Délai avant de cacher l'image (4 secondes dans cet exemple)
 
   setTimeout(function() {
     image.style.display = 'none'; // Cache complètement l'image en la masquant
   }, 6000); // Délai avant de masquer complètement l'image (6 secondes dans cet exemple)
 });
 
+console.log()
+
+const divToClick = document.querySelectorAll('.play-sound-click')
+
+const sound = new Audio("./sound.mp3")
+const playSound = () => {console.log(divToClick);}
+divToClick.addEventListener('click', playSound(sound))
