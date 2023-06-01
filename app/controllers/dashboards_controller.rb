0747cbf6a0b97865affe672505418c705cd03207
Jonathan Magnet <j.magnet@outlook.com>
2023-06-01 15:31:22 +0200

    page-bookings

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,4 +3,10 @@ class DashboardsController < ApplicationController
     user = current_user.id
     @bookings = Booking.where(user_id: user)
   end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to dashboards_path
+  end
 end
