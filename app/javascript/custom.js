fd73ca0742a4c6bcf57821da0ff8791309b24ae6
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-06-02 13:21:44 +0200

    ok

---
+++ app/javascript/custom.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function() {
+  // var playSoundButton = document.getElementById('playSoundButton');
+  // var audio = new Audio('<%= asset_path('sounds/mysound.mp3') %>');
+
+  // playSoundButton.addEventListener('click', () => {
+  //   audio.play();
+  //   console.log("hello");
+  // });
+// });
