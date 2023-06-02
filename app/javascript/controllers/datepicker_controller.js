f105008ff02a241af3ab55d665dde6bbd1151b92
Kenza Bouachik <kenzabouachik@gmail.com>
2023-06-02 14:03:10 +0200

    added price

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,9 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+import rangePlugin from "flatpickrRangePlugin"
+
+const dates = {start: ""}
 
 export default class extends Controller {
+  static targets = ["start", "end", "price"]
+  static values = {
+    price: Number
+  }
+
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.startTarget, {
+      "plugins": [new rangePlugin({ input: this.endTarget})]
+    })
+
+  }
+
+  setStart() {
+    dates.start = this.startTarget.value
+  }
+
+  calculatePrice() {
+   const start = this.startTarget.value
+   const end = this.endTarget.value
+   const interval = (new Date(end) - new Date(start)) / (1000 * 3600 * 24)
+   this.priceTarget.innerHTML = `<btn class="btn-ghost">${interval}-nuits-${interval*this.priceValue}€</btn>`
   }
 }
